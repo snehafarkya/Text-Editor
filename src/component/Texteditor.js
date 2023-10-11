be69d41cfbf0aeba83e6f39695f9163ae438dc29
snehafarkya <snhafarkya@gmail.com>
2023-10-11 18:34:45 +0530

    image resized

--- src/component/Texteditor.js
+++ src/component/Texteditor.js
@@ -1,13 +1,15 @@
 import React from "react";
 import 'quill/dist/quill.snow.css'
-import ReactQuill from 'react-quill'
+import ReactQuill, { Quill } from 'react-quill'
 import { useState } from "react";
+import ImageResize from 'quill-image-resize-module-react';
+Quill.register('modules/imageResize', ImageResize);
 
 const TextEditor = () => {
   const [show,setShow] = useState(false)
   var modules = {
     toolbar: [
-      [{ size: ["small", false, "large", "huge"] }],
+      [{ size: ["small", false, "large", "huge"] },{ font: [] }],
       ["bold", "italic", "underline", "strike", "blockquote"],
       // [{ list: "ordered" }, { list: "bullet" }],
       ["link", "image", "video"],
@@ -19,11 +21,16 @@ const TextEditor = () => {
         { align: [] }
       ],
       [{ "color": ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }],
-    ]
+    ],
+    imageResize: {
+      parchment: Quill.import('parchment'),
+      modules: ['Resize', 'DisplaySize']
+    }
+  
   };
-
+ 
   var formats = [
-    "header", "height", "bold", "italic",
+    "header","font", "height", "bold", "italic",
     "underline", "strike", "blockquote",
     "list", "color", "bullet", "indent",
     "link", "image", "align", "size", "video"
@@ -31,7 +38,7 @@ const TextEditor = () => {
 
   const handleProcedureContentChange = (content) => {
     console.log("content---->", content);
-    var res  = document.getElementById('result').innerHTML = content
+    // var res  = document.getElementById('result').innerHTML = content
     // res.style.display = "none"
 
     
@@ -64,7 +71,7 @@ const TextEditor = () => {
       
 
     </div>
-      <button className="px-4 py-2 bg-slate-200 cursor-pointer mt-16 hover:bg-slate-300 " onClick={()=>setShow(true)}>Save</button>
+      {/* <button className="px-4 py-2 bg-slate-200 cursor-pointer mt-16 hover:bg-slate-300 " onClick={()=>setShow(true)}>Save</button> */}
       {show && (
            <div className="w-[60%] justify-center mt-20 mx-auto bg-gray-200 " id="result"></div>
 
