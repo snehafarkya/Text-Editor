1e259ec3f92d2b0d3b04e30b10687544848842b3
snehafarkya <snhafarkya@gmail.com>
2023-10-11 18:41:39 +0530

    background added

--- src/component/Texteditor.js
+++ src/component/Texteditor.js
@@ -20,7 +20,7 @@ const TextEditor = () => {
         { indent: "+1" },
         { align: [] }
       ],
-      [{ "color": ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] }],
+      [{ "color": ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color'] },{"background": ["#000000", "#e60000", "#ff9900", "#ffff00", "#008a00", "#0066cc", "#9933ff", "#ffffff", "#facccc", "#ffebcc", "#ffffcc", "#cce8cc", "#cce0f5", "#ebd6ff", "#bbbbbb", "#f06666", "#ffc266", "#ffff66", "#66b966", "#66a3e0", "#c285ff", "#888888", "#a10000", "#b26b00", "#b2b200", "#006100", "#0047b2", "#6b24b2", "#444444", "#5c0000", "#663d00", "#666600", "#003700", "#002966", "#3d1466", 'custom-color']}],
     ],
     imageResize: {
       parchment: Quill.import('parchment'),
@@ -33,12 +33,12 @@ const TextEditor = () => {
     "header","font", "height", "bold", "italic",
     "underline", "strike", "blockquote",
     "list", "color", "bullet", "indent",
-    "link", "image", "align", "size", "video"
+    "link", "image", "align", "background","size", "video"
   ];
 
   const handleProcedureContentChange = (content) => {
     console.log("content---->", content);
-    // var res  = document.getElementById('result').innerHTML = content
+    var res  = document.getElementById('result').innerHTML = content
     // res.style.display = "none"
 
     
@@ -72,10 +72,9 @@ const TextEditor = () => {
 
     </div>
       {/* <button className="px-4 py-2 bg-slate-200 cursor-pointer mt-16 hover:bg-slate-300 " onClick={()=>setShow(true)}>Save</button> */}
-      {show && (
            <div className="w-[60%] justify-center mt-20 mx-auto bg-gray-200 " id="result"></div>
 
-      )}
+      
       </>
   );
 
