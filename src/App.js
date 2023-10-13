0906bf4f6fa645fbd9e23002c0c6f30edb32c1c7
snehafarkya <snhafarkya@gmail.com>
2023-10-13 12:09:07 +0530

    home added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import TextEditor from './component/Texteditor';
+import Home from './component/Home';
 
 function App() {
   return (
     <div className="App">
+      <Home/>
       <TextEditor/>
     </div>
   );
---
+++ src/component/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+      <div className="home flex flex-col mx-auto items-center justify-center gap-3">
+      <p className='text-blue-300 text-5xl '> Give your thoughts a wing to <span className='font-semibold text-blue-200 underline'>FLY</span></p>
+        <p className='text-blue-300 text-2xl '> A platform for you, by you , from you </p>
+        <img src="https://o.remove.bg/downloads/daf547d2-f7d7-4b67-825b-d601362ddd64/copywriter-huge-laptop-writing-creative-promotional-text-copywriting-job-home-based-copywriter-freelance-copywriting-concept-illustration_335657-2375-removebg-preview.png" alt="" className='mt-4 rounded-xl' />
+        
+      </div>
+    </div>
+  )
+}
